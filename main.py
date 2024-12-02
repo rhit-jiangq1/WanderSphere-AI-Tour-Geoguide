b83e2e09f194bbf0a4463d56dd1fea22e3c1d73f
rhit-jiangq1 <73331485+rhit-jiangq1@users.noreply.github.com>
2024-12-02 15:52:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from instagram_api import InstagramAPI
 from place_finder_app import PlaceFinderApp
 
-ACCESS_TOKEN = "YOUR_ACCESS_TOKEN_HERE"  # I don't have a Instagram account for Instagram API yet
+ACCESS_TOKEN = "YOUR_ACCESS_TOKEN_HERE"  # I don't have a Facebook Meta Developer Account account for Instagram API yet. The website told me should be done by 12/3/2024
 
 def main():
     root = tk.Tk()
